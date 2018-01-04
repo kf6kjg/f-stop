aa18e325a23803ba9c76951a7509e75eb2bc087c
Ricky C <kf6kjg@users.noreply.github.com>
2018-01-03 19:04:33 -0800

    Corrected stock response message creation so that it actually is sent

--- Source/LibF_Stop/F_StopRouter.cs
+++ Source/LibF_Stop/F_StopRouter.cs
@@ -145,22 +145,13 @@ namespace LibF_Stop {
 					return (Response)"";
 				}
 			};
-
-			// /CAPS/HTT/ADDCAP/{TOKEN}/{CAP_UUID}/{BANDWIDTH}
-			// /CAPS/HTT/REMCAP/{TOKEN}/{CAP_UUID}
-			// /CAPS/HTT/PAUSE/{TOKEN}/{CAP_UUID}
-			// /CAPS/HTT/RESUME/{TOKEN}/{CAP_UUID}
-			// /CAPS/HTT/LIMIT/{TOKEN}/{CAP_UUID}/{BANDWIDTH}
-			// /CAPS/HTT/{CAP_UUID}/?texture_id={TEXTUREUUID}
-			// /CAPS/HTT/{CAP_UUID}/?mesh_id={TEXTUREUUID}
-
 		}
 
 		private static class StockReply {
 			public static Response Ok = new Response {
 				ContentType = "text/html",
 				StatusCode = HttpStatusCode.OK,
-				Contents = (obj) => obj.Write(Encoding.UTF8.GetBytes(@""), 0, 0),
+				//Contents = stream => (new System.IO.StreamWriter(stream) { AutoFlush = true }).Write(@""),
 				Headers = new Dictionary<string, string> {
 					{"Content-Length", "0"},
 				}
@@ -169,46 +160,46 @@ namespace LibF_Stop {
 			public static Response BadRequest = new Response {
 				ContentType = "text/html",
 				StatusCode = HttpStatusCode.BadRequest,
-				Contents = (obj) => obj.Write(Encoding.UTF8.GetBytes(@"<html>
+				Contents = stream => (new System.IO.StreamWriter(stream) { AutoFlush = true }).Write(@"<html>
 <head><title>Bad Request</title></head>
 <body><h1>400 Bad Request</h1></body>
-</html>"), 0, 0),
+</html>"),
 			};
 
 			public static Response NotFound = new Response {
 				ContentType = "text/html",
 				StatusCode = HttpStatusCode.NotFound,
-				Contents = (obj) => obj.Write(Encoding.UTF8.GetBytes(@"<html>
+				Contents = stream => (new System.IO.StreamWriter(stream) { AutoFlush = true }).Write(@"<html>
 <head><title>Not Found</title></head>
 <body><h1>404 Not Found</h1></body>
-</html>"), 0, 0),
+</html>"),
 			};
 
 			public static Response ServiceUnavailable = new Response {
 				ContentType = "text/html",
 				StatusCode = HttpStatusCode.ServiceUnavailable,
-				Contents = (obj) => obj.Write(Encoding.UTF8.GetBytes(@"<html>
+				Contents = stream => (new System.IO.StreamWriter(stream) { AutoFlush = true }).Write(@"<html>
 <head><title>Service Unavailable</title></head>
 <body><h1>503 Service Unavailable</h1></body>
-</html>"), 0, 0),
+</html>"),
 			};
 
 			public static Response InternalServerError = new Response {
 				ContentType = "text/html",
 				StatusCode = HttpStatusCode.InternalServerError,
-				Contents = (obj) => obj.Write(Encoding.UTF8.GetBytes(@"<html>
+				Contents = stream => (new System.IO.StreamWriter(stream) { AutoFlush = true }).Write(@"<html>
 <head><title>Internal Server Error</title></head>
 <body><h1>500 Internal Server Error</h1></body>
-</html>"), 0, 0),
+</html>"),
 			};
 
 			public static Response RangeError = new Response {
 				ContentType = "text/html",
 				StatusCode = HttpStatusCode.RequestedRangeNotSatisfiable,
-				Contents = (obj) => obj.Write(Encoding.UTF8.GetBytes(@"<html>
+				Contents = stream => (new System.IO.StreamWriter(stream) { AutoFlush = true }).Write(@"<html>
 <head><title>Requested Range not satisfiable</title></head>
 <body><h1>416 Requested Range not satisfiable</h1></body>
-</html>"), 0, 0),
+</html>"),
 			};
 		}
 	}
