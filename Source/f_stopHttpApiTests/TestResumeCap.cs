9be0e17bae00a1b8cbb2e294dc93a162f1df11b7
Ricky C <kf6kjg@users.noreply.github.com>
2018-01-25 21:34:40 -0800

    API tests for resume cap

--- Source/f_stopHttpApiTests/TestResumeCap.cs
+++ Source/f_stopHttpApiTests/TestResumeCap.cs
@@ -23,15 +23,94 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using NUnit.Framework;
 using System;
+using System.IO;
+using System.Net;
+using InWorldz.Data.Assets.Stratus;
+using NUnit.Framework;
+using RestSharp;
 
 namespace f_stopHttpApiTests {
 	[TestFixture]
 	public class TestResumeCap {
-		// TODO: Tests for API cap resume
+		private StratusAsset _knownTextureAsset;
+
+		public static IRestResponse ResumeCap(Guid capId, string adminToken = Constants.SERVICE_ADMIN_TOKEN) {
+			var client = new RestClient(Constants.SERVICE_URI);
+			var url = $"/CAPS/HTT/RESUME/{adminToken}/{capId.ToString("N")}";
+			var request = new RestRequest(url, Method.GET);
+			var response = client.Execute(request);
+			return response;
+		}
+
+		[OneTimeTearDown]
+		public void CleanupCache() {
+			try {
+				Directory.Delete(Constants.TEST_CACHE_PATH, true);
+			}
+			catch (DirectoryNotFoundException) {
+			}
+		}
+
+		[OneTimeSetUp]
+		public void Setup() {
+			CleanupCache();
+
+			// Using hardcoded GUIDs to make debugging easier.
+
+			_knownTextureAsset = TestGetAsset.CreateAndCacheAsset(
+				"_knownTextureAsset",
+				0,
+				new byte[] { 0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20, 0x0D, 0x0A, 0x87, 0x0A }, // JPEG-2000 magic numbers
+				Guid.Parse("01000000-0000-0000-0000-000000000000")
+			);
+		}
+
 		[Test]
-		public void TestCase() {
+		public void TestResumeCapAllowsGet() {
+			var capId = Guid.NewGuid();
+			TestAddCap.AddCap(capId);
+			TestPauseCap.PauseCap(capId);
+			ResumeCap(capId);
+			try {
+				var response = TestGetAsset.GetAsset(capId, _knownTextureAsset.Id, timeout: TimeSpan.FromMilliseconds(100));
+				Assert.Pass();
+			}
+			catch (WebException e) {
+				Assert.AreEqual(WebExceptionStatus.Timeout, e.Status); // It timed out
+			}
+		}
+
+		[Test]
+		public void TestResumeCapBadAdminTokenBadRequest() {
+			var capId = Guid.NewGuid();
+			TestAddCap.AddCap(capId);
+			TestPauseCap.PauseCap(capId);
+			var response = ResumeCap(capId, "badToken");
+			Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode, "Bad Status:\n\n" + response.Content);
+		}
+
+		[Test]
+		public void TestResumeCapNotPausedOk() {
+			var capId = Guid.NewGuid();
+			TestAddCap.AddCap(capId);
+			var response = ResumeCap(capId);
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Bad Status:\n\n" + response.Content);
+		}
+
+		[Test]
+		public void TestResumeCapPausedOk() {
+			var capId = Guid.NewGuid();
+			TestAddCap.AddCap(capId);
+			TestPauseCap.PauseCap(capId);
+			var response = ResumeCap(capId);
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Bad Status:\n\n" + response.Content);
+		}
+
+		[Test]
+		public void TestResumeCapUnknownCapBadRequest() {
+			var response = ResumeCap(Guid.NewGuid());
+			Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode, "Bad Status:\n\n" + response.Content);
 		}
 	}
 }
